b3f3b965eed9438f187e89ef54d9b0d4340204ae
mido <mihai.dobre@cegeka.com>
2016-08-05 18:31:06 +0300

    added markdown support. added url filtering. only clean urls are included in doc.

--- rest_framework_docs/api_docs.py
+++ rest_framework_docs/api_docs.py
@@ -1,3 +1,4 @@
+import re
 from importlib import import_module
 from django.conf import settings
 from django.core.urlresolvers import RegexURLResolver, RegexURLPattern
@@ -27,8 +28,9 @@ class ApiDocumentation(object):
                 parent_pattern = None if pattern._regex == "^" else pattern
                 self.get_all_view_names(urlpatterns=pattern.url_patterns, parent_pattern=parent_pattern)
             elif isinstance(pattern, RegexURLPattern) and self._is_drf_view(pattern) and not self._is_format_endpoint(pattern):
-                api_endpoint = ApiEndpoint(pattern, parent_pattern, self.drf_router)
-                self.endpoints.append(api_endpoint)
+                if re.match('^\^([\w]+/)+\$$', pattern.regex.pattern):
+                    api_endpoint = ApiEndpoint(pattern, parent_pattern, self.drf_router)
+                    self.endpoints.append(api_endpoint)
 
     def _is_drf_view(self, pattern):
         """
