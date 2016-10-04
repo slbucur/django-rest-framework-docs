c2fb61cf05f41458fcc456bb12cb5d9a68c44e16
mido <mihai.dobre@cegeka.com>
2016-10-04 13:47:07 +0300

    removed comments

--- rest_framework_docs/static/rest_framework_docs/js/components/liveapi.js
+++ rest_framework_docs/static/rest_framework_docs/js/components/liveapi.js
@@ -26,17 +26,11 @@ var LiveAPIEndpoints = React.createClass({
 
     var self = this;
     var request = this.refs.request.state;
-    console.log('request')
-    console.log(request)
     var headers = {};
     if (this.refs.request.state.headers.authorization) {
       headers['Authorization'] = this.refs.request.state.headers.authorization;
     };
-    console.log('headers')
-    console.log(headers)
     var data = this.getData();
-    console.log('data')
-    console.log(data)
     
     // Now Make the Request
     APIRequest(request.selectedMethod, request.endpoint.path)
@@ -55,9 +49,8 @@ var LiveAPIEndpoints = React.createClass({
         <div className="modal-body">
           <div className="row">
             <div id="request" className="col-md-6 request">
-              <Request endpoint={this.state.endpoint} ref='request' />
-            console.log(this.state.endpoint)
-           </div>
+              <Request endpoint={this.state.endpoint} ref='request' />            
+            </div>
             <div id="response" className="col-md-6 response">
               <Response payload={this.state.response} />
             </div>
