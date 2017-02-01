c045bd4631a112825965bea2ce6425938c4b4f32
Stefan-Lucian Bucur <slbucur@gmail.com>
2017-02-01 15:22:38 +0200

    Setting for ignore regex.

--- rest_framework_docs/settings.py
+++ rest_framework_docs/settings.py
@@ -6,7 +6,8 @@ class DRFSettings(object):
     def __init__(self):
         self.drf_settings = {
             "HIDE_DOCS": self.get_setting("HIDE_DOCS") or False,
-            "IGNORE_SUB_FIELDS": self.get_setting("IGNORE_SUB_FIELDS") or False
+            "IGNORE_URL_REGEX": self.get_setting("IGNORE_SUB_FIELDS") or None,
+            "INCLUDE_ONLY_URLS": self.get_setting("INCLUDE_ONLY_URLS") or None,
         }
 
     def get_setting(self, name):
