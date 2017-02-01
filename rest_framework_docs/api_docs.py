2d4e39f94ff7a91d71d30af94c8330e63e422139
Stefan-Lucian Bucur <slbucur@gmail.com>
2017-02-01 15:45:35 +0200

    Maybe it works.

--- rest_framework_docs/api_docs.py
+++ rest_framework_docs/api_docs.py
@@ -22,21 +22,24 @@ class ApiDocumentation(object):
         else:
             self.get_all_view_names(root_urlconf.urlpatterns)
 
-    def get_all_view_names(self, urlpatterns, parent_pattern=None):
+    def get_all_view_names(self, urlpatterns, parent_pattern=None, parent_pattern_so_far=''):
         ignored_namespaces = ['dynamic_data']
         settings = DRFSettings().settings
         only_urls = settings['INCLUDE_ONLY_URLS']
+
         for pattern in urlpatterns:
+            pattern_so_far = parent_pattern_so_far + pattern.regex.pattern
             # if only_urls exists, ignore urls that are not in it
             print only_urls
             print pattern.regex.pattern
-            if only_urls and not [only_url for only_url in only_urls if only_url in pattern.regex.pattern]:
+            if only_urls and not [only_url for only_url in only_urls if only_url in pattern_so_far]:
                 continue
             if [pattern for ignored_ns in ignored_namespaces if ignored_ns in pattern.regex.pattern]:
                 continue
             if isinstance(pattern, RegexURLResolver):
                 parent_pattern = None if pattern._regex == "^" else pattern
-                self.get_all_view_names(urlpatterns=pattern.url_patterns, parent_pattern=parent_pattern)
+                self.get_all_view_names(urlpatterns=pattern.url_patterns, parent_pattern=parent_pattern,
+                                        parent_pattern_so_far=pattern_so_far)
             elif isinstance(pattern, RegexURLPattern) and self._is_drf_view(pattern) and not self._is_format_endpoint(pattern):
                     
                 if not settings['IGNORE_URL_REGEX'] or re.match(settings['IGNORE_URL_REGEX'], pattern.regex.pattern):
